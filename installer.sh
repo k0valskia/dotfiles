71cd6cb392cfd0e11afbc539e21e650ead2be5f6
Herlangga Maulani <maulani.morningstar@gmail.com>
2022-12-02 01:13:26 +0700

    first commit

--- installer.sh
+++ installer.sh
@@ -2,5 +2,5 @@ sudo apt install neovim vim i3 picom zsh kitty nitrogen
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mv .oh-my-zsh $HOME
 mv .zshrc $HOME
-cp -r * ~/.config/
+mv * ~/.config/
 chsh -s /usr/bin/zsh
