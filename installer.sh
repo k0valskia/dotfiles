fd65a3d00e62a354f22e04401d55d023cd9d69cf
Herlangga Maulani <maulani.morningstar@gmail.com>
2022-12-02 00:41:11 +0700

    add script

---
+++ installer.sh
@@ -0,0 +1,6 @@
+sudo apt install neovim vim i3 picom zsh kitty nitrogen
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv .oh-my-zsh $HOME
+mv .zshrc $HOME
+cp -r * ~/.config/
+chsh -s /usr/bin/zsh
