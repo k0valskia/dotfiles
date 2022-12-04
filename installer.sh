b32f471611f69764acb111c0c16a479caca79987
Herlangga Maulani <maulani.morningstar@gmail.com>
2022-12-04 14:28:15 +0700

    delete installer script

--- installer.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install neovim vim i3 picom zsh kitty nitrogen
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mv .oh-my-zsh $HOME
-mv .zshrc $HOME
-mv * ~/.config/
-chsh -s /usr/bin/zsh
